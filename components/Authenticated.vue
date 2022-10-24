dc1ec97b7964248ceb0e4c5ed2f6b65e099bbfb1
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-10-24 17:58:37 +0200

    Redesign of page to allow for more content

--- components/Authenticated.vue
+++ components/Authenticated.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="w-full max-w-5xl mx-auto bg-white px-5 py-4 rounded shadow-xl">
+    <div class="w-full max-w-5xl mx-auto bg-white px-5 py-4 rounded-t shadow-xl">
       <div class="flex items-center justify-between">
         <div class="flex items-center space-x-2">
           <img
-              class="w-12 h-12 rounded"
+              class="w-10 h-10"
               src="https://avatars.githubusercontent.com/u/25911230?v=4"
               alt="User Avatar"
           />
@@ -11,20 +11,9 @@
             Authenticated as Nils!
           </h1>
         </div>
-        <button type="button" @click="$emit('logout')">
-          Sign out
+        <button class="flex items-center justify-center space-x-2 bg-red-500 text-white rounded-lg py-2 px-3 text-lg" @click="$emit('logout')">
+          <span>Logout</span>
         </button>
       </div>
-    </div>
-    <div class="bg-white max-w-5xl w-full mx-auto rounded shadow-lg py-4 px-5 overflow-y-auto">
-          <pre class="bg-gray-800 text-gray-200 p-4 rounded shadow my-2 overflow-x-auto">
-{
-  "user": {
-    "name": "Nils",
-    "email": "demo@demo.com",
-    "image": "https://avatars.githubusercontent.com/u/25911230?v=4"
-  },
-  "expires": "2022-11-23T12:59:46.226Z"
-}</pre>
     </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-3">
+    <h1 class="text-2xl mb-2">
+      Welcome to the <span class="text-gray-200 bg-gray-800 py-0.5 px-1.5 font-mono rounded">nuxt-user</span> demo!
+    </h1>
+    <p>
+      This page show the features of the module as well as code snippets of how to integrate it into your own project.
+    </p>
+    <p>
+      To get started click "login" at the bottom of this page.
+    </p>
+    <br />
+    <p>
+      If you run into any issues you can view our documentation here, or open an issue.
+    </p>
+  </div>
+</template>
