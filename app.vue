29d772c12302de2e719bcaaf00dd18abf4062123
Valia Fetisov <contact@valiafetisov.com>
2022-11-02 18:27:58 +0100

    working docker-compose with ssl

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen h-full relative bg-gray-100 pb-20">
     <Navbar />
     <div class="fixed bottom-0 w-full">
-      <Authenticated v-if="status === 'authenticated'" :user-data="data.user" @signOut="() => signOut({ callbackUrl: '/' })" />
+      <Authenticated v-if="data && status === 'authenticated'" :user-data="data.user" @signOut="() => signOut({ callbackUrl: '/' })" />
       <NotAuthenticated v-else @signIn="signIn" />
     </div>
     <Welcome />
