e752cb960ff57df9c8a9ca7af31f163868c65184
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-11-02 21:50:16 +0100

    Changed name from nuxt-user to nuxt-auth

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-900">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <a href="https://github.com/sidebase/nuxt-user" target="_blank" class="flex items-center">
-        <span class="self-center text-xl font-semibold whitespace-nowrap text-white">🔐 nuxt-user</span>
+        <span class="self-center text-xl font-semibold whitespace-nowrap text-white">🔐 nuxt-auth</span>
       </a>
       <div class="w-full md:w-auto" id="navbar-default">
         <ul class="flex flex-col md:p-4 mt-4 py-2 bg-gray-50 rounded-lg border md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
@@ -42,7 +42,7 @@
           <li class="flex items-center ml-2 md:space-x-4">
             <a
                 target="_blank"
-                href="https://github.com/sidebase/nuxt-user"
+                href="https://github.com/sidebase/nuxt-auth"
                 class="block py-2 pr-4 pl-3 rounded md:border-0 md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent"
             >
               <i class="fa-brands fa-github text-lg pt-[1px]" />
