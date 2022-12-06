94b21dc6a17640e8582f607c6b24268826898615
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-12-06 14:29:44 +0100

    Added locally unprotected route and API routes pages

---
+++ components/api/APITableRow.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  props: {
+    route: String,
+    method: String,
+    link: String,
+  }
+}
+</script>
+
+<template>
+  <tr class="bg-gray-800 border-gray-700">
+    <th scope="row" class="py-4 px-6 font-medium whitespace-nowrap text-white">
+      {{ route }}
+    </th>
+    <td class="py-4 px-6 uppercase">
+      {{ method }}
+    </td>
+    <td class="py-4 px-6">
+      <nuxt-link :href="link" target="_blank">
+        View Route
+        <i class="fa fa-external-link" />
+      </nuxt-link>
+    </td>
+  </tr>
+</template>
---
+++ pages/api-routes.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="max-w-5xl mx-auto mt-5 px-5">
+    <div class="overflow-x-auto relative rounded">
+      <table class="w-full text-sm text-left text-gray-400">
+        <thead class="text-xs uppercase bg-gray-700 text-gray-400">
+        <tr>
+          <th scope="col" class="py-3 px-6">
+            Route
+          </th>
+          <th scope="col" class="py-3 px-6">
+            Method
+          </th>
+          <th scope="col" class="py-3 px-6">
+            Link
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+          <APITableRow
+              route="/session"
+              method="get"
+              link="/api/auth/session"
+          />
+          <APITableRow
+              route="/csrf"
+              method="get"
+              link="/api/auth/csrf"
+          />
+          <APITableRow
+              route="/providers"
+              method="get"
+              link="/api/auth/providers"
+          />
+          <APITableRow
+            route="/signin"
+            method="get"
+            link="/api/auth/signin"
+          />
+          <APITableRow
+              route="/signin/:provider"
+              method="post"
+              link="/api/auth/signin/github"
+          />
+          <APITableRow
+              route="/callback/:provider"
+              method="get & post"
+              link="/api/auth/callback/github"
+          />
+          <APITableRow
+              route="/signout"
+              method="get & post"
+              link="/api/auth/signout"
+          />
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({ auth: false })
+</script>
