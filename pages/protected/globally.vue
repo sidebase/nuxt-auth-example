ad02905e0136a528252b2cbdbdce406cf6830f53
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-12-06 15:36:55 +0100

    Added extra protection

--- pages/protected/globally.vue
+++ pages/protected/globally.vue
@@ -1,3 +1,8 @@
 <template>
-  <Secret protection-type="global" href="https://github.com/sidebase/nuxt-auth#global-middleware" />
+  <Secret v-if="status === 'authenticated'" protection-type="global" href="https://github.com/sidebase/nuxt-auth#global-middleware" />
+  <Protected v-else />
 </template>
+
+<script lang="ts" setup>
+const { status } = useSession()
+</script>
