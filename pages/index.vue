8e9c698b59d3d38578dd0ba9ceb2d558aff0263f
colton <colton.padden@fastmail.com>
2023-06-14 16:09:32 -0400

    useSession() -> useAuth()

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
 
 <script lang="ts" setup>
 definePageMeta({ auth: false })
-const { data, status, getCsrfToken, getProviders } = useSession()
+const { data, status, getCsrfToken, getProviders } = useAuth()
 
 const providers = await getProviders()
 const csrfToken = await getCsrfToken()
