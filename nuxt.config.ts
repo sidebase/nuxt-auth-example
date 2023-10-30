c4fd2efeee2f896c5a2b7c32424a81790e8a00ab
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-10-30 14:43:46 +0100

    updated to local provider

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,32 @@ export default defineNuxtConfig({
         '@fortawesome/fontawesome-free/css/all.min.css'
     ],
     modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
+    build: {
+        transpile: ['jsonwebtoken']
+      },
     auth: {
-        origin: process.env.ORIGIN,
-        enableGlobalAppMiddleware: true
+        provider: {
+          type: 'local',
+          endpoints: {
+            getSession: { path: '/user' }
+          },
+          pages: {
+            login: '/'
+          },
+          token: {
+            signInResponseTokenPointer: '/token/accessToken'
+          },
+          sessionDataType: { id: 'string', email: 'string', name: 'string', role: 'admin | guest | account', subscriptions: "{ id: number, status: 'ACTIVE' | 'INACTIVE' }[]" }
+        },
+        session: {
+          // Whether to refresh the session every time the browser window is refocused.
+          enableRefreshOnWindowFocus: true,
+    
+          // Whether to refresh the session every `X` milliseconds. Set this to `false` to turn it off. The session will only be refreshed if a session already exists.
+          enableRefreshPeriodically: 5000
+        },
+        globalAppMiddleware: {
+          isEnabled: true
+        }
     }
 })
--- server/api/healthz.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler((event) => {
-    return {
-        service: 'online'
-    }
-})
