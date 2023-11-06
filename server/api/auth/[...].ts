4ba6a1eeba8b7205c85839dda69875a3a5e10481
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-11-06 16:41:17 +0100

    add a auth secret

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,7 +4,7 @@ import { NuxtAuthHandler } from '#auth'
 
 export default NuxtAuthHandler({
   // TODO: SET A STRONG SECRET, SEE https://sidebase.io/nuxt-auth/configuration/nuxt-auth-handler#secret
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.AUTH_SECRET || 'my-auth-secret',
   // TODO: ADD YOUR OWN AUTHENTICATION PROVIDER HERE, READ THE DOCS FOR MORE: https://sidebase.io/nuxt-auth
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
